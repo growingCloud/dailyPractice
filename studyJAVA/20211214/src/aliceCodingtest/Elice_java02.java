e4a8e560a9df13b6e59d256597f7a0dfd5f1ecde
growingCloud <96115415+growingCloud@users.noreply.github.com>
2021-12-16 07:46:04 +0900

    21년 12월 14일에 연습했던 백준, 엘리스 아카데미 코딩테스트 입니다

--- studyJAVA/20211214/src/aliceCodingtest/Elice_java02.java
+++ studyJAVA/20211214/src/aliceCodingtest/Elice_java02.java
@@ -1,5 +1,28 @@
 package aliceCodingtest;
 
 public class Elice_java02 {
+	public static void main(String[] args) {
+        // System.out.println() 그리고 사칙연산을 이용해 
+		// 아래 “결과”와 같은 출력이 되도록 2, 3번째 줄에 있는 x, y값을 결정해주세요.’
+        int x = 6;
+        int y = 3;
 
+        // 더하기 연산
+        int my_sum = x + y;
+        System.out.println("my_sum : " + my_sum);
+
+        // 빼기 연산
+        int my_sub = x - y;
+        System.out.println("my_sub : " + my_sub);
+
+        // 곱하기 연산
+        int my_mul = x * y;
+        System.out.println("my_mul : " + my_mul);
+
+        // 나누기 연산
+        int my_div = x / y;
+        System.out.println("my_div : " + my_div);
+
+	}
+	
 }
