a667a544ff634bdd6d574cd5a8a12d238b265df3
growingCloud <96115415+growingCloud@users.noreply.github.com>
2022-01-02 09:29:14 +0900

    사전 토이프로젝트 연습 (update)

--- studyPython/20220101_BoardWithPython/app.py
+++ studyPython/20220101_BoardWithPython/app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, jsonify, request, abort, redirect, url
 from datetime import datetime
 from bson.objectid import ObjectId  # {'_id': ObjectId(idx)}에서 idx를 ObjectId의 형태로 받기 위한 모듈
 import time
+import math
 
 app = Flask(__name__)
 
@@ -20,11 +21,11 @@ def home():
 # current_time(datetime)을 우리가 보는 시간으로 바꿔주는 함수
 # 이렇게 번거로운 과정을 거치는 이유? -> (내 생각) mongo에는 가공하기 쉽게 밀리초 단위의 시간으로 저장해뒀다가 꺼낼때 우리가 보는 시간으로 바꿔주기 위해!
 @app.template_filter('format_datetime')
-def format_datetime(value) :
-    if value is None :
-        return ''   # 만약 시간값이 없다면 공백을 반환
+def format_datetime(value):
+    if value is None:
+        return ''  # 만약 시간값이 없다면 공백을 반환
 
-    now_timestamp = time.time()     # offset = utc time과 한국의 time 시차 (+9:00)
+    now_timestamp = time.time()  # offset = utc time과 한국의 time 시차 (+9:00)
     offset = datetime.fromtimestamp(now_timestamp) - datetime.utcfromtimestamp(now_timestamp)
     value = datetime.fromtimestamp((int(value) / 1000)) + offset
     return value.strftime('%Y-%m-%d %H:%M:%S')
@@ -33,7 +34,7 @@ def format_datetime(value) :
 # 게시글 작성 (Create)
 @app.route('/write', methods=['GET', 'POST'])
 def board_write():
-    if request.method == "POST" :
+    if request.method == "POST":
         # 내용이 입력되어 있는 상태에서는 POST로 받음
         name_receive = request.form['name_give']
         title_receive = request.form['title_give']
@@ -58,7 +59,7 @@ def board_write():
         # print(idx.inserted_id)
         return redirect(url_for("board_view", idx=idx.inserted_id))
 
-    else :
+    else:
         # /write 경로로 들어오면 GET으로 받아 입력창 보여줌
         return render_template('write.html')
 
@@ -67,12 +68,13 @@ def board_write():
 @app.route('/view', methods=['GET'])
 def board_view():
     idx = request.args.get("idx")
-    if idx is not None :
+    if idx is not None:
         data = db.board.find_one({'_id': ObjectId(idx)})
 
-        if data is not None :
+        if data is not None:
             view_data = {
                 # 'id': data.get('_id'), >> bson 타입이라 제외함 (objectid is not json serializable)
+                'primary': str('_id'),
                 'name': data.get('name'),
                 'title': data.get('title'),
                 'contents': data.get('contents'),
@@ -81,7 +83,58 @@ def board_view():
             }
 
             return jsonify({'result': 'success', 'view_datas': view_data})
-    return abort(404)   # 맞는 페이지가 없을때 404 페이지 내보내기
+    return abort(404)  # 맞는 페이지가 없을때 404 페이지 내보내기
+
+
+# 게시글 리스트 페이지 (Read_List)
+@app.route('/list', methods=['GET'])
+def board_lists():
+# 페이지 기능 구현 (1)
+    # 페이지 값, 값이 없는 경우 기본값은 1
+    page = request.args.get("page", 1, type=int)
+    # 페이지 당 게시물 출력 갯수 (현재 10개로 설정)
+    limit = request.args.get("limit", 10, type=int)
+
+# 검색 기능 구현
+    search = request.args.get("search", -1, type=int)
+    keyword = request.args.get("keyword", type=str)
+
+    query = {}      # 최종적으로 완성된 쿼리를 만들 변수
+    search_list = []
+
+    # 게시글 검색 기능의 option value (0, 1, 2, 3)
+    if search == 0:
+        search_list.append({"title": {"$regex": keyword}})           # '안녕하세요' 중에서 '녕'자를 찾아주는 기능 (중간 검색)
+    elif search == 1:
+        search_list.append({"contents": {"$regex": keyword}})
+    elif search == 2:
+        search_list.append({"title": {"$regex": keyword}})
+        search_list.append({"contents": {"$regex": keyword}})
+    elif search == 3:
+        search_list.append({"name": {"$regex": keyword}})
+
+    # 검색 대상이 하나라도 존재 할 경우, query 변수에 $or 리스트를 쿼리한다
+    if len(search_list) > 0 :
+        query = {"$or" : search_list}
+    print(query)
+
+# 페이지 기능 구현 (2)
+    lists = list(db.board.find({}, {'_id': False})
+                 .skip((page - 1) * limit).limit(limit))
+
+    # 페이지네이션 구현 (라이브러리 사용X)
+    # total_count = db.board.find({}).count()                 # 게시물의 총 갯수
+    # last_page_num = math.ceil(total_count / limit)          # 마지막 페이지의 수, 게시물이 하나라도 있으면 페이지가 존재해야 하므로 소수점이 생기면 무조건 올림!
+    # block_size = 5                                          # 페이지 블럭을 5개씩 지정
+    # block_num = int((page - 1) / block_size)                # 현재 게시글 블럭의 위치
+    # block_start = int((block_size * block_num) + 1)         # 블럭의 시작 위치
+    # block_end = math.ceil((block_start + block_size - 1))   # 블럭의 끝 위치
+
+    # page_nation = (page, limit, total_count, last_page_num, block_size, block_num, block_start, block_end)
+
+    # return jsonify({'postings': lists, 'making-pages': page_nation})
+    return jsonify({'postings': lists})
+
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5005, debug=True)
--- studyPython/20220101_BoardWithPython/google.py
+++ studyPython/20220101_BoardWithPython/google.py
@@ -6,23 +6,21 @@ from pymongo import MongoClient
 from datetime import datetime
 import random
 
-# 몽고DB
-client = MongoClient(host="localhost", port=27017)
-# myweb 데이터베이스
-db = client.myweb
-# board 컬렉션
-col = db.board
+# 몽고DB, dbpractice 데이터베이스
+client = MongoClient('localhost', 27017)
+db = client.dbpractice
 
 # 구글 검색시 헤더값을 설저하지 않으면 브라우저에서 보이는것과 다른 결과가 나옴
-header = {"user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36"}
+header = {
+    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36"}
 
 # 검색 결과의 5페이지까지만 수집
 for i in range(6):
-	# 구글 검색 URL, 검색어는 파이썬
+    # 구글 검색 URL, 검색어는 파이썬
     url = "https://www.google.com/search?q={}&start={}".format("파이썬", i * 10)
     # url 접속
     r = requests.get(url, headers=header)
-    
+
     # 웹페이지의 검색 결과를 파싱하기 위한 준비
     # lxml 라이브러리 사용(설치 요망)
     bs = BeautifulSoup(r.text, "lxml")
@@ -31,22 +29,21 @@ for i in range(6):
     lists = bs.select("div.g")
 
     # 검색결과 루프
-    for l in lists:
-    	# 게시물 작성시간 기록을 위해 현재시간 저장 (utc 타임)
+    for li in lists:
+        # 게시물 작성시간 기록을 위해 현재시간 저장 (utc 타임)
         current_utc_time = round(datetime.utcnow().timestamp() * 1000)
 
         try:
-        	# 검색 결과의 제목은 h3 태그의 LC20lb 클래스에 있음
-            title = l.select_one("h3.LC20lb").text
+            # 검색 결과의 제목은 h3 태그의 LC20lb 클래스에 있음
+            title = li.select_one("h3.LC20lb").text
 
             # 검색결과의 요약내용은 div 태그의 s 클래스에 있음
-            contents = l.select_one("div.s").text
+            contents = li.select_one("div.s").text
 
-            # 몽고DB에 저장
-            # 작성자와 writer_id 설정 필요
-            col.insert_one({
+            # 몽고DB에 저장, 작성자와 writer_id 설정 필요
+            db.board.insert_one({
                 "name": "테스터",
-                "writer_id": "",
+                # "writer_id": "",
                 "title": title,
                 "contents": contents,
                 "view": random.randrange(30, 777),
